0b8f6606528120bdb781d1c0773f8102fc7d38c7
f2c-demo <f2c_demo@163.com>
2015-11-11 17:47:01 +0800

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-processesNum=`ps aux | grep SimpleHTTPServer | grep 8080 | grep -v grep | wc -l | sed 's/ //g'`
+processesNum=`ps aux | grep SimpleHTTPServer | grep 9527 | grep -v grep | wc -l | sed 's/ //g'`
 if [ "$processesNum" == "1" ];then
     exit 0
 else 
